6ca1b210267224754dee65c508900ed1f9df49c6
montero <kuantro04@gmail.com>
2025-04-24 12:43:19 +0300

    init

---
+++ src/main/java/com/monntterro/trelloflowbot/TrelloFlowBotApplication.java
@@ -0,0 +1,12 @@
+package com.monntterro.trelloflowbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloFlowBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloFlowBotApplication.class, args);
+    }
+}
