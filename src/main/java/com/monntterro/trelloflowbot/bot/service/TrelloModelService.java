41d9e63d2f808cfe405e7b422fa312cff2a8b9b7
montero <kuantro04@gmail.com>
2025-05-04 10:32:38 +0300

    feat: sequential registration fix: saving models to db

--- src/main/java/com/monntterro/trelloflowbot/bot/service/TrelloModelService.java
+++ src/main/java/com/monntterro/trelloflowbot/bot/service/TrelloModelService.java
@@ -1,14 +1,13 @@
 package com.monntterro.trelloflowbot.bot.service;
 
 import com.monntterro.trelloflowbot.bot.entity.trellomodel.TrelloModel;
-import com.monntterro.trelloflowbot.bot.entity.trellomodel.Type;
 import com.monntterro.trelloflowbot.bot.entity.user.User;
 import com.monntterro.trelloflowbot.bot.repository.TrelloModelRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,20 +21,31 @@ public class TrelloModelService {
         return trelloModelRepository.findByModelIdAndUser(modelId, user);
     }
 
-    @Transactional
-    public List<TrelloModel> findAllByUserAndType(User user, Type modelType) {
-        return userService.findById(user.getId()).stream()
-                .flatMap(u -> u.getTrelloModels().stream())
-                .filter(model -> model.getType() == modelType)
-                .sorted(Comparator.comparing(TrelloModel::isSubscribed).reversed())
-                .toList();
-    }
-
     public void save(TrelloModel trelloModel) {
         trelloModelRepository.save(trelloModel);
     }
 
-    public List<TrelloModel> saveAll(List<TrelloModel> trelloModels) {
-        return trelloModelRepository.saveAll(trelloModels);
+    @Transactional
+    public List<TrelloModel> saveAllOrUpdate(List<TrelloModel> trelloModels, User user) {
+        List<TrelloModel> existingTrelloModels = userService.findById(user.getId())
+                .orElseThrow(() -> new RuntimeException("User not found"))
+                .getTrelloModels();
+
+        List<TrelloModel> updatedTrelloModels = new ArrayList<>();
+        for (TrelloModel trelloModel : trelloModels) {
+            Optional<TrelloModel> existingModel = existingTrelloModels.stream()
+                    .filter(model -> model.getModelId().equals(trelloModel.getModelId()))
+                    .findFirst();
+
+            if (existingModel.isPresent()) {
+                TrelloModel modelToUpdate = existingModel.get();
+                modelToUpdate.setName(trelloModel.getName());
+                modelToUpdate.setUrl(trelloModel.getUrl());
+                updatedTrelloModels.add(modelToUpdate);
+            } else {
+                trelloModelRepository.save(trelloModel);
+            }
+        }
+        return trelloModelRepository.saveAll(updatedTrelloModels);
     }
 }
