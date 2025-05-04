fc43a2430af5a362c0c50b7afe1733b267bdf49e
montero <kuantro04@gmail.com>
2025-05-04 15:51:16 +0300

    fix: default language

--- src/main/java/com/monntterro/trelloflowbot/bot/utils/MessageResource.java
+++ src/main/java/com/monntterro/trelloflowbot/bot/utils/MessageResource.java
@@ -1,24 +1,27 @@
 package com.monntterro.trelloflowbot.bot.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
 @Component
+@RequiredArgsConstructor
 public class MessageResource {
     private final MessageSource messageSource;
 
-    @Autowired
-    public MessageResource(MessageSource messageSource) {
-        this.messageSource = messageSource;
-    }
-
     public String getMessage(String code, String language, Object... args) {
         if (args.length == 0) {
-            return messageSource.getMessage(code, null, Locale.forLanguageTag(language));
+            return messageSource.getMessage(code, null, toLocale(language));
         }
-        return messageSource.getMessage(code, args, Locale.forLanguageTag(language));
+        return messageSource.getMessage(code, args, toLocale(language));
+    }
+
+    private Locale toLocale(String language) {
+        if (language == null || language.isEmpty()) {
+            return Locale.ROOT;
+        }
+        return Locale.forLanguageTag(language);
     }
 }
